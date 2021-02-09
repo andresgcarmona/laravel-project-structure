91961a479a113b02a77145f231902e68fdb61098
GitHub Action <action@github.com>
2021-02-09 22:27:35 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/andresgcarmona/laravelprojectstructure/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.andresgcarmona.laravelprojectstructure.services
+
+import com.github.andresgcarmona.laravelprojectstructure.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/andresgcarmona/laravelprojectstructure/services/MyProjectService.kt
+++ src/main/kotlin/com/github/andresgcarmona/laravelprojectstructure/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.andresgcarmona.laravelprojectstructure.services
 
+import com.github.andresgcarmona.laravelprojectstructure.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
